9c2083d58243b821faed3604a6cce4ea5463672f
engelsun <artyom.romenskiy@gmail.com>
2018-10-13 12:01:50 +0300

    init project

---
+++ src/main/java/com/engelsun/resttesttask/RestTestTaskApplication.java
@@ -0,0 +1,12 @@
+package com.engelsun.resttesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTestTaskApplication.class, args);
+	}
+}
